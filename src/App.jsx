1d952a815a743b9562ba2531034556cc2684fd50
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2025-01-03 17:17:03 +0800

    Create header and hamburger menu structure and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
-
+import { useState } from "react";
+import "./index.css";
 
 function App() {
-  
+	const [showMenu, setShowMenu] = useState(false);
 
-  return (
-    <>
-      <h1>Hi</h1>
-    </>
-  )
+	return (
+		<>
+			<header>
+				<div className="container header-flex">
+					<div className="header__logo">
+						<h1>Syakir</h1>
+					</div>
+					<div className={`header__links ${showMenu ? "active" : "hide"}`}>
+						<a href="">Home</a>
+						<a href="">About</a>
+						<a href="">Projects</a>
+						<a href="">Contact</a>
+					</div>
+					<a
+						className="burger-menu"
+						href=""
+						onClick={(e) => {
+							e.preventDefault();
+
+							setShowMenu((prev) => !prev);
+						}}
+					>
+						Burger
+					</a>
+				</div>
+			</header>
+		</>
+	);
 }
 
-export default App
+export default App;
